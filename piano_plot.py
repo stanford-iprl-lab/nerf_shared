72e8868a21026a3d5dbc4b0ff1fc9b68eba51f08
Michal Adamkiewicz <mikh3x4@gmail.com>
2021-08-22 23:56:25 +0200

    working piano plot

--- piano_plot.py
+++ piano_plot.py
@@ -11,48 +11,13 @@ from typeguard import typechecked
 
 patch_typeguard()
 
-def main():
-    start_state = torch.tensor([0,0, 1.0,  0, 0,0,   0  ,0  ,0, 0.0,0.0,0])
-    end_state   = torch.tensor([5,0,1.0,  0,-0,0,  0  ,0  ,0, 0.0,0.0,0])
-
-    steps = 20
-
-    traj = Trajectory(start_state, end_state, steps)
-
-    opt = torch.optim.Adam(traj.params(), lr=0.1)
-    # scheduler = torch.optim.lr_scheduler.ExponentialLR(opt, gamma=0.99)
-    # opt = torch.optim.Adam([traj.states], lr=0.1)
-    # opt = torch.optim.Adam([traj.actions], lr=0.1)
-
-    # act = torch.tensor([ 20.0, -5.0,0,1])
-    # state = start_state.clone()
-    # states = traj.states.clone().detach()
-    # for i in range(steps - 2):
-    #     state = next_state(state, act)
-    #     states[i] = state.clone()
-    # traj.states = states
 
 
-    for it in range(500):
-        opt.zero_grad()
-        loss = traj.total_cost()
-        print(it, loss)
-        loss.backward()
-        # print(traj.actions.grad)
+class System:
 
-        opt.step()
-        # scheduler.step()
-
-    fig = plt.figure(figsize=plt.figaspect(2.))
-    ax3d = fig.add_subplot(2, 1, 1, projection='3d')
-    ax2d = fig.add_subplot(2, 1, 2)
-    traj.plot(ax3d)
-    traj.plot_action(ax2d) 
-    plt.show()
-
-
-class Trajectory:
     def __init__(self, start_state, end_state, steps):
+        self.dt = 0.1
+
         self.start_state = start_state[None,:]
         self.end_state = end_state[None,:]
 
@@ -62,216 +27,131 @@ class Trajectory:
         # self.states = torch.tensor(states, requires_grad=True)
         self.states = states.clone().detach().requires_grad_(True)
 
-        self.actions = torch.zeros(steps-1, 4, requires_grad=True)
-        # self.actions = torch.randn(steps-1, 4, requires_grad=True)
+    def params(self):
+        return [self.states]
 
-    def plot_action(self, ax):
-        actions = self.actions.detach().numpy() 
-        ax.plot(actions[...,0], label="fz")
-        ax.plot(actions[...,1], label="tx")
-        ax.plot(actions[...,2], label="ty")
-        ax.plot(actions[...,3], label="tz")
+    def get_states(self):
+        return torch.cat( [self.start_state, self.states, self.end_state], dim=0)
 
-        states = self.states.detach().numpy()
-        ax.plot(states[...,0], label="px")
-        ax.plot(states[...,4], label="vx")
-        ax.plot(states[...,7], label="ey")
+    def get_actions(self):
+        states = self.get_states()
+        prev_state = states[:-1, :]
+        next_state = states[1:, :]
+
+        middle_rot = (prev_state[:, 2] + next_state[:,2])/2
+        rot_matrix = self.rot_matrix(-middle_rot) # inverse because world -> body
+
+        lin_vel = rot_matrix @ (next_state[:, :2] - prev_state[:, :2])[...,None] / self.dt
+        lin_vel = lin_vel[...,0]
+
+        rot_vel = (next_state[:, 2:] - prev_state[:,2:])/self.dt
+
+        return torch.cat( [lin_vel, rot_vel], dim=-1 )
+
+    def get_cost(self):
+        actions = self.get_actions()
+
+        x = actions[:, 0]**2
+        y = actions[:, 1]**2
+        a = actions[:, 2]**2
+
+        return y*10 + a*0.1 + 0.01*x
+
+    def total_cost(self):
+        return torch.sum(self.get_cost())
 
 
-        cost = []
-        states = self.all_states()
-        for action, state, n_state in zip(self.actions, states[:-1], states[1:]):
-            pred_state = next_state(state,action)
-            cost.append( torch.sum( torch.abs( pred_state - n_state) ).detach() )
-        ax.plot(cost, label="cost")
+    @staticmethod
+    @typechecked
+    def rot_matrix(angle: TensorType["batch":...]) -> TensorType["batch":..., 2, 2]:
+        rot_matrix = torch.zeros( angle.shape + (2,2) )
+        rot_matrix[:, 0,0] =  torch.cos(angle)
+        rot_matrix[:, 0,1] = -torch.sin(angle)
+        rot_matrix[:, 1,0] =  torch.sin(angle)
+        rot_matrix[:, 1,1] =  torch.cos(angle)
+        return rot_matrix
 
+    def plot(self, fig = None):
+        if fig == None:
+            fig = plt.figure(figsize=plt.figaspect(2.))
+        ax_map = fig.add_subplot(2, 1, 1)
+        ax_graph = fig.add_subplot(2, 1, 2)
+        self.plot_map(ax_map)
+        self.plot_graph(ax_graph) 
+        plt.show()
+
+    def plot_graph(self, ax):
+        states = self.get_states().detach().numpy()
+        ax.plot(states[...,0], label="x")
+        ax.plot(states[...,1], label="y")
+        ax.plot(states[...,2], label="a")
+        actions = self.get_actions().detach().numpy() 
+        ax.plot(actions[...,0], label="dx")
+        ax.plot(actions[...,1], label="dy")
+        ax.plot(actions[...,2], label="da")
+
+        ax.plot(self.get_cost().detach().numpy(), label="cost")
         ax.legend()
 
-    def plot(self, ax):
-        states = self.all_states()
-        pos = states[..., :3]
-        rot = states[..., 6:9]
+    def plot_map(self, ax):
+        states = self.get_states()
 
-        rot_matrix = vec_to_rot_matrix(rot).detach().numpy()
-        pos = pos.detach().numpy()
+        pos = states[..., :2]
+        rot = states[..., 2]
 
-        # ax.axis('equal')
-        ax.set_xlim3d(0, 10)
-        ax.set_ylim3d(-5, 5)
-        ax.set_zlim3d(-5, 5)
+        rot_matrix = self.rot_matrix(rot)
+
+        rot_matrix = rot_matrix.detach().numpy()
+        pos        = pos.detach().numpy()
+
+        ax.set_aspect('equal')
+        ax.set_xlim(-1, 5)
+        ax.set_ylim(-1, 5)
         ax.plot( * pos.T )
 
         size = 0.5
 
         # create point for origin, plus a right-handed coordinate indicator.
-        points = np.array( [[0, 0, 0], [size, 0, 0], [0, size, 0], [0, 0, size]])
+        points = np.array( [[0, 0], [size, 0], [0, size]])
 
-        colors = ["r", "g", "b"]
+        colors = ["r", "b"]
 
-        # P, 3, 4          =  P, 3, 3   @ 3, 4     + P, 3, _
+        # P, 2, 3          =  P, 2, 2   @ 2, 3     + P, 2, _
         points_world_frame = rot_matrix @ points.T + pos[..., None]
-        # print(points_world_frame.shape)
 
         for p in range(pos.shape[0]):
-            for i in range(1, 4):
+            for i in range(1, 3):
                 ax.plot(
                         points_world_frame[p, 0, [0,i]],
                         points_world_frame[p, 1, [0,i]],
-                        points_world_frame[p, 2, [0,i]],
                     c=colors[i - 1],
                 )
 
 
+def main():
+    start_state = torch.tensor([0,0,0])
+    # end_state   = torch.tensor([3,3, np.pi/2])
+    # end_state   = torch.tensor([3,3, 0])
+    end_state   = torch.tensor([0,3, 0.01])
 
-    def params(self):
-        return [self.actions, self.states]
+    steps = 20
 
-    def all_states(self):
-        return torch.cat( [self.start_state, self.states, self.end_state], dim=0)
+    traj = System(start_state, end_state, steps)
 
-    def dynamics_cost(self):
-        cost = 0
-        states = self.all_states()
-        steps = states.shape[0]
-        # for action, state, n_state in zip(self.actions, states[:-1], states[1:]):
-        #     pred_state = next_state(state,action)
-        #     cost += torch.sum( torch.abs( pred_state - n_state) )
+    opt = torch.optim.Adam(traj.params(), lr=0.05)
 
-        # for a1,a2,a3, state, n_state in zip(self.actions[:-2],self.actions[1:-1],self.actions[2:], states[:-3], states[3:]):
-        #     pred_state = next_state(next_state(next_state(state,a1),a2),a3)
-        #     cost += torch.sum( torch.abs( pred_state - n_state) )
+    for it in range(500):
+        opt.zero_grad()
+        loss = traj.total_cost()
+        print(it, loss)
+        loss.backward()
 
-        # parabola = (states[...,0] - 5)**2  - states[...,2] - 3
-        # cost += torch.sum(F.relu(parabola))
+        opt.step()
 
-        depth = 2
-        for i in range(steps-depth):
-            actions = self.actions[i:i+depth]
-            local_states = states[i:i+depth+1]
-            state = local_states[0]
-            future_states = local_states[1:]
-
-            for action, compare_state in zip(actions, future_states):
-                state = next_state(state, action)
-                cost += torch.sum( torch.abs(state - compare_state) )
-
-        return cost
-
-    def action_cost(self):
-        cost = 0
-        for action in self.actions:
-            cost += action[0]**2
-            cost += torch.sum( (action[1:])**2 )
-        return cost
-
-    def total_cost(self):
-        return self.dynamics_cost()# + 0.1* self.action_cost()
-
-@typechecked
-def rot_matrix_to_vec( R: TensorType["batch":..., 3, 3]) -> TensorType["batch":..., 3]:
-    batch_dims = R.shape[:-2]
-
-    trace = torch.diagonal(R, dim1=-2, dim2=-1).sum(-1)
-
-    def acos_safe(x, eps=1e-4):
-        """https://github.com/pytorch/pytorch/issues/8069"""
-        slope = np.arccos(1-eps) / eps
-        # TODO: stop doing this allocation once sparse gradients with NaNs (like in
-        # th.where) are handled differently.
-        buf = torch.empty_like(x)
-        good = abs(x) <= 1-eps
-        bad = ~good
-        sign = torch.sign(x[bad])
-        buf[good] = torch.acos(x[good])
-        buf[bad] = torch.acos(sign * (1 - eps)) - slope*sign*(abs(x[bad]) - 1 + eps)
-        return buf
-
-    angle = acos_safe((trace - 1) / 2)[..., None]
-    # print(trace, angle)
-
-    vec = (
-        1
-        / (2 * torch.sin(angle + 1e-5))
-        * torch.stack(
-            [
-                R[..., 2, 1] - R[..., 1, 2],
-                R[..., 0, 2] - R[..., 2, 0],
-                R[..., 1, 0] - R[..., 0, 1],
-            ],
-            dim=-1,
-        )
-    )
-
-    # needed to overwrite nanes from dividing by zero
-    vec[angle[..., 0] == 0] = torch.zeros(3, device=R.device)
-
-    # eg TensorType["batch_size", "views", "max_objects", 3, 1]
-    rot_vec = (angle * vec)[...]
-
-    return rot_vec
-
-@typechecked
-def vec_to_rot_matrix(rot_vec: TensorType["batch":..., 3]) -> TensorType["batch":..., 3,3]:
-    assert not torch.any(torch.isnan(rot_vec))
-
-    angle = torch.norm(rot_vec, dim=-1, keepdim=True)
-    axis = rot_vec / (1e-5 + angle)
-    S = skew_matrix(axis)
-    # print(S.shape)
-    # print(angle.shape)
-    angle = angle[...,None]
-    rot_matrix = (
-            torch.eye(3)
-            + torch.sin(angle) * S
-            + (1 - torch.cos(angle)) * S @ S
-            )
-    return rot_matrix
-
-@typechecked
-def skew_matrix(vec: TensorType["batch":..., 3]) -> TensorType["batch":..., 3,3]:
-    batch_dims = vec.shape[:-1]
-    S = torch.zeros(*batch_dims, 3, 3)
-    S[..., 0, 1] = -vec[..., 2]
-    S[..., 0, 2] =  vec[..., 1]
-    S[..., 1, 0] =  vec[..., 2]
-    S[..., 1, 2] = -vec[..., 0]
-    S[..., 2, 0] = -vec[..., 1]
-    S[..., 2, 1] =  vec[..., 0]
-    return S
+    traj.plot()
 
 
-def next_state(state, action):
-    #TODO batch this
-    pos = state[...,0:3]
-    v   = state[...,3:6]
-    euler_vector = state[...,6:9]
-    omega = state[...,9:12]
 
-    fz = action[...,0, None]
-    torque = action[...,1:4]
-
-    e3 = torch.tensor([0,0,1])
-
-    mass = 1
-    dt = 0.1
-    g = -10
-    J = torch.eye(3)
-    J_inv = torch.linalg.inv(J)
-
-    R = vec_to_rot_matrix(euler_vector)
-
-    dv = g * e3 + R @ (fz * e3) / mass
-    domega = J_inv @ torque - J_inv @ skew_matrix(omega) @ J @ omega
-    dpos = v
-
-    next_v = dv * dt + v
-    next_euler = rot_matrix_to_vec(R @ vec_to_rot_matrix(omega * dt))
-    # next_euler = rot_matrix_to_vec(R @ vec_to_rot_matrix(omega * dt) @ vec_to_rot_matrix(domega * dt**2))
-    next_omega = domega * dt + omega
-    next_pos = dpos * dt + pos# + 0.5 * dv * dt**2
-
-    next_state = torch.cat([next_pos, next_v, next_euler, next_omega], dim=-1)
-    return next_state
 
 
 if __name__ == "__main__":
