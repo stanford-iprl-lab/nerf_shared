70602f9f77fe5b8441b670b78b2a2fd35aa09be2
Michal Adamkiewicz <mikh3x4@gmail.com>
2021-08-24 03:26:43 +0200

    body to world cleanup

--- piano_plot.py
+++ piano_plot.py
@@ -30,8 +30,6 @@ def plot_nerf(ax, nerf):
     density = density.detach().numpy()
 
     ax.pcolormesh(coods[...,0],coods[...,1],  density, cmap = cm.binary, shading='auto')
-    # plt.pcolormesh(coods[...,0],coods[...,1],  density, cmap = cm.viridis)
-
 
 class System:
     def __init__(self, start_state, end_state, steps):
@@ -72,14 +70,17 @@ class System:
 
         return torch.cat( [lin_vel, rot_vel], dim=-1 )
 
-    def get_hitpoints(self) -> TensorType["states", "points", 2]:
+
+    @typechecked
+    def body_to_world(self, points: TensorType["batch", 2]) -> TensorType["states", "batch", 2]:
         states = self.get_states()
         pos = states[..., :2]
         rot = states[..., 2]
 
-        # S, 2, P      S, 2, 2               2, P                S, 2, _
-        body_points = self.rot_matrix(rot) @ self.robot_body.T + pos[..., None]
+        # S, 2, P      S, 2, 2               2, P       S, 2, _
+        body_points = self.rot_matrix(rot) @ points.T + pos[..., None]
         return body_points.swapdims(-1,-2)
+
 
     def get_cost(self):
         actions = self.get_actions()
@@ -91,7 +92,7 @@ class System:
         pos = self.get_states()[:-1, :2]
 
         distance = (x**2 + y**2)**0.5 * self.dt
-        density = nerf( self.get_hitpoints()[1:,...] )**2
+        density = nerf( self.body_to_world(self.robot_body)[1:,...] )**2
 
         colision_prob = torch.mean( density, dim = -1) * distance
 
@@ -140,36 +141,27 @@ class System:
         ax.set_xlim(-5, 5)
         ax.set_ylim(-5, 5)
 
-        states = self.get_states()
-        pos = states[..., :2]
-        rot = states[..., 2]
-
-        rot_matrix = self.rot_matrix(rot)
-
-        rot_matrix = rot_matrix.detach().numpy()
-        pos        = pos.detach().numpy()
-
         # PLOT PATH
+        pos = self.body_to_world( torch.zeros((1,2))).detach().numpy()
         ax.plot( * pos.T )
 
         # PLOTS BODY POINTS
         # S, P, 2
-        body_points = self.get_hitpoints().detach().numpy()
+        body_points = self.body_to_world(self.robot_body).detach().numpy()
         for state_body in body_points:
             ax.plot( *state_body.T, "g.", ms=72./ax.figure.dpi, alpha = 0.5)
 
         # PLOTS AXIS
-        # create point for origin, plus a right-handed coordinate indicator.
         size = 0.5
-        points = np.array( [[0, 0], [size, 0], [0, size]])
+        points = torch.tensor( [[0, 0], [size, 0], [0, size]])
         colors = ["r", "b"]
 
-        # S, 2, 3          =  S, 2, 2   @ 2, 3     + S, 2, _
-        points_world_frame = rot_matrix @ points.T + pos[..., None]
-        for p in range(pos.shape[0]):
+        # S, 3, 2
+        points_world_frame = self.body_to_world(points).detach().numpy()
+        for state_axis in points_world_frame:
             for i in range(1, 3):
-                ax.plot(points_world_frame[p, 0, [0,i]],
-                        points_world_frame[p, 1, [0,i]],
+                ax.plot(state_axis[[0,i], 0],
+                        state_axis[[0,i], 1],
                         c=colors[i - 1],)
 
 
