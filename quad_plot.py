636842c56a7592b8a96657352152f16f29346ad4
Michal Adamkiewicz <mikh3x4@gmail.com>
2021-08-24 03:29:50 +0200

    quad clean up

--- quad_plot.py
+++ quad_plot.py
@@ -13,24 +13,12 @@ from typeguard import typechecked
 patch_typeguard()
 
 @typechecked
-def nerf(points: TensorType["batch":..., 2]) -> TensorType["batch":...]:
-    x = points[..., 0]
-    y = points[..., 1]
-
-    sharpness = 8
-    return torch.sigmoid(sharpness * (y-1 )) * torch.sigmoid(sharpness * (x-1 )) 
+def nerf(points: TensorType["batch":..., 3]) -> TensorType["batch":...]:
+    pass
 
 
 def plot_nerf(ax, nerf):
-    linspace = torch.linspace(-5,5, 100)
-
-    # 50, 50, 2
-    coods = torch.stack( torch.meshgrid( linspace, linspace ), dim=-1)
-    density = nerf(coods)
-    density = density.detach().numpy()
-
-    ax.pcolormesh(coods[...,0],coods[...,1],  density, cmap = cm.binary, shading='auto')
-    # plt.pcolormesh(coods[...,0],coods[...,1],  density, cmap = cm.viridis)
+    pass
 
 
 class System:
@@ -43,43 +31,48 @@ class System:
         slider = torch.linspace(0, 1, steps)[1:-1, None]
 
         states = (1-slider) * start_state + slider * end_state
-        # self.states = torch.tensor(states, requires_grad=True)
         self.states = states.clone().detach().requires_grad_(True)
 
-        body = torch.stack( torch.meshgrid( torch.linspace(-0.5, 0.5, 10), 
-                                            torch.linspace(-  1,   1, 10) ), dim=-1)
+        # body = torch.stack( torch.meshgrid( torch.linspace(-0.5, 0.5, 10), 
+        #                                     torch.linspace(-  1,   1, 10) ), dim=-1)
+        # self.robot_body = body.reshape(-1, 2)
+        self.robot_body = torch.zeros(1,3)
 
-        self.robot_body = body.reshape(-1, 2)
 
     def params(self):
         return [self.states]
 
     def get_states(self):
-        return torch.cat( [self.start_state, self.states, self.end_state], dim=0)
+        # start and end states doubled to enforce zero velocity constraint
+        return torch.cat( [self.start_state, self.start_state, self.states, self.end_state, self.end_state], dim=0)
 
     def get_actions(self):
+        g = torch.tensor([0,0,-10])
+
         states = self.get_states()
         prev_state = states[:-1, :]
         next_state = states[1:, :]
 
-        middle_rot = (prev_state[:, 2] + next_state[:,2])/2
-        rot_matrix = self.rot_matrix(-middle_rot) # inverse because world -> body
+        diff = (next_state - prev_state)/self.dt
+        vel = diff[..., :3]
+        z_world_rate = diff[..., 3:]
 
-        lin_vel = rot_matrix @ (next_state[:, :2] - prev_state[:, :2])[...,None] / self.dt
-        lin_vel = lin_vel[...,0]
+        prev_vel = vel[:-1, :]
+        next_vel = vel[1:, :]
 
-        rot_vel = (next_state[:, 2:] - prev_state[:,2:])/self.dt
+        target_accel = (next_vel - prev_vel)/self.dt - g
+        z_thrust     = torch.norm(target_accel, dim=-1, keepdim=True)
 
-        return torch.cat( [lin_vel, rot_vel], dim=-1 )
+        z_axis_body = target_accel/z_thrust
+        z_angle = states[1:-1,3]
+        #TODO
 
-    def get_hitpoints(self) -> TensorType["states", "points", 2]:
-        states = self.get_states()
-        pos = states[..., :2]
-        rot = states[..., 2]
 
-        # S, 2, P      S, 2, 2               2, P                S, 2, _
-        body_points = self.rot_matrix(rot) @ self.robot_body.T + pos[..., None]
-        return body_points.swapdims(-1,-2)
+    @typechecked
+    def body_to_world(self, points: TensorType["batch":..., 3]) -> TensorType["states", "batch":..., 3]:
+        pass
+
+
 
     def get_cost(self):
         actions = self.get_actions()
@@ -101,16 +94,6 @@ class System:
         return torch.sum(self.get_cost())
 
 
-    @staticmethod
-    @typechecked
-    def rot_matrix(angle: TensorType["batch":...]) -> TensorType["batch":..., 2, 2]:
-        rot_matrix = torch.zeros( angle.shape + (2,2) )
-        rot_matrix[:, 0,0] =  torch.cos(angle)
-        rot_matrix[:, 0,1] = -torch.sin(angle)
-        rot_matrix[:, 1,0] =  torch.sin(angle)
-        rot_matrix[:, 1,1] =  torch.cos(angle)
-        return rot_matrix
-
     def plot(self, fig = None):
         if fig == None:
             fig = plt.figure(figsize=plt.figaspect(2.))
@@ -123,61 +106,54 @@ class System:
         plt.show()
 
     def plot_graph(self, ax):
-        states = self.get_states().detach().numpy()
-        ax.plot(states[...,0], label="x")
-        ax.plot(states[...,1], label="y")
-        ax.plot(states[...,2], label="a")
-        actions = self.get_actions().detach().numpy() 
-        ax.plot(actions[...,0], label="dx")
-        ax.plot(actions[...,1], label="dy")
-        ax.plot(actions[...,2], label="da")
+        actions = self.actions.detach().numpy() 
+        ax.plot(actions[...,0], label="fz")
+        ax.plot(actions[...,1], label="tx")
+        ax.plot(actions[...,2], label="ty")
+        ax.plot(actions[...,3], label="tz")
+
+        # states = self.states.detach().numpy()
+        # ax.plot(states[...,0], label="px")
+        # ax.plot(states[...,4], label="vx")
+        # ax.plot(states[...,7], label="ey")
 
         ax.plot(self.get_cost().detach().numpy(), label="cost")
         ax.legend()
 
     def plot_map(self, ax):
-        ax.set_aspect('equal')
-        ax.set_xlim(-5, 5)
-        ax.set_ylim(-5, 5)
-
-        states = self.get_states()
-        pos = states[..., :2]
-        rot = states[..., 2]
-
-        rot_matrix = self.rot_matrix(rot)
-
-        rot_matrix = rot_matrix.detach().numpy()
-        pos        = pos.detach().numpy()
+        ax.set_xlim3d(0, 10)
+        ax.set_ylim3d(-5, 5)
+        ax.set_zlim3d(-5, 5)
 
         # PLOT PATH
+        pos = self.body_to_world( torch.zeros((1,3))).detach().numpy()
         ax.plot( * pos.T )
 
         # PLOTS BODY POINTS
         # S, P, 2
-        body_points = self.get_hitpoints().detach().numpy()
+        body_points = self.body_to_world( self.body_points ).detach().numpy()
         for state_body in body_points:
             ax.plot( *state_body.T, "g.", ms=72./ax.figure.dpi, alpha = 0.5)
 
         # PLOTS AXIS
         # create point for origin, plus a right-handed coordinate indicator.
         size = 0.5
-        points = np.array( [[0, 0], [size, 0], [0, size]])
-        colors = ["r", "b"]
+        points = np.array( [[0, 0, 0], [size, 0, 0], [0, size, 0], [0, 0, size]])
+        colors = ["r", "g", "b"]
 
-        # S, 2, 3          =  S, 2, 2   @ 2, 3     + S, 2, _
-        points_world_frame = rot_matrix @ points.T + pos[..., None]
-        for p in range(pos.shape[0]):
-            for i in range(1, 3):
-                ax.plot(points_world_frame[p, 0, [0,i]],
-                        points_world_frame[p, 1, [0,i]],
-                        c=colors[i - 1],)
+        # S, 4, 2
+        points_world_frame = self.body_to_world(points).detach().numpy()
+        for state_axis in points_world_frame:
+            for i in range(1, 4):
+                ax.plot(state_axis[[0,i], 0],
+                        state_axis[[0,i], 1],
+                        state_axis[[0,i], 2],
+                    c=colors[i - 1],)
 
 
 def main():
-    start_state = torch.tensor([4,0,0])
-    # end_state   = torch.tensor([3,3, np.pi/2])
-    # end_state   = torch.tensor([3,3, 0])
-    end_state   = torch.tensor([0,4, 0.01])
+    start_state = torch.tensor([0,0,1, 0 ])
+    end_state   = torch.tensor([10,0,1 0.01])
 
     steps = 20
 
