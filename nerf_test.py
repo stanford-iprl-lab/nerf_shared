fc679f1eb2a20de2d0d78ff7e3a7900cc14510b7
Michal Adamkiewicz <mikh3x4@gmail.com>
2021-08-27 06:21:48 +0200

    clean nerf test

--- nerf_test.py
+++ nerf_test.py
@@ -4,183 +4,115 @@ import torch
 
 import matplotlib.pyplot as plt
 
-# from run_nerf_helpers import create_nerf
 from nerf_core import create_nerf
 from render_functions import Renderer
 
-def config_parser():
+import argparse
 
-    import configargparse
-    parser = configargparse.ArgumentParser()
-    parser.add_argument('--config', is_config_file=True, 
-                        help='config file path')
-    parser.add_argument("--expname", type=str, 
-                        help='experiment name')
-    parser.add_argument("--basedir", type=str, default='./logs/', 
-                        help='where to store ckpts and logs')
-    parser.add_argument("--datadir", type=str, default='./data/llff/fern', 
-                        help='input data directory')
+from torchtyping import TensorType, patch_typeguard
+from typeguard import typechecked
 
-    # training options
-    parser.add_argument("--netdepth", type=int, default=8, 
-                        help='layers in network')
-    parser.add_argument("--netwidth", type=int, default=256, 
-                        help='channels per layer')
-    parser.add_argument("--netdepth_fine", type=int, default=8, 
-                        help='layers in fine network')
-    parser.add_argument("--netwidth_fine", type=int, default=256, 
-                        help='channels per layer in fine network')
-    parser.add_argument("--N_rand", type=int, default=32*32*4, 
-                        help='batch size (number of random rays per gradient step)')
-    parser.add_argument("--lrate", type=float, default=5e-4, 
-                        help='learning rate')
-    parser.add_argument("--lrate_decay", type=int, default=250, 
-                        help='exponential learning rate decay (in 1000 steps)')
-    parser.add_argument("--chunk", type=int, default=1024*32, 
-                        help='number of rays processed in parallel, decrease if running out of memory')
-    parser.add_argument("--netchunk", type=int, default=1024*64, 
-                        help='number of pts sent through network in parallel, decrease if running out of memory')
-    parser.add_argument("--no_batching", action='store_true', 
-                        help='only take random rays from 1 image at a time')
-    parser.add_argument("--no_reload", action='store_true', 
-                        help='do not reload weights from saved ckpt')
-    parser.add_argument("--ft_path", type=str, default=None, 
-                        help='specific weights npy file to reload for coarse network')
+patch_typeguard()
 
-    # rendering options
-    parser.add_argument("--N_samples", type=int, default=64, 
-                        help='number of coarse samples per ray')
-    parser.add_argument("--N_importance", type=int, default=0,
-                        help='number of additional fine samples per ray')
-    parser.add_argument("--perturb", type=float, default=1.,
-                        help='set to 0. for no jitter, 1. for jitter')
-    parser.add_argument("--use_viewdirs", action='store_true', 
-                        help='use full 5D input instead of 3D')
-    parser.add_argument("--i_embed", type=int, default=0, 
-                        help='set 0 for default positional encoding, -1 for none')
-    parser.add_argument("--multires", type=int, default=10, 
-                        help='log2 of max freq for positional encoding (3D location)')
-    parser.add_argument("--multires_views", type=int, default=4, 
-                        help='log2 of max freq for positional encoding (2D direction)')
-    parser.add_argument("--raw_noise_std", type=float, default=0., 
-                        help='std dev of noise added to regularize sigma_a output, 1e0 recommended')
+def get_args():
+    settings = {
+            "N_importance":128,
+            "N_rand":1024,
+            "N_samples":64,
+            "basedir":'./logs',
+            "chunk":32768,
+            "config":'configs/playground.txt',
+            "datadir":'./data/nerf_synthetic/playground',
+            "dataset_type":'blender',
+            "expname":'playground_test',
+            "factor":8,
+            "ft_path":None,
+            "half_res":False,
+            "i_embed":0,
+            "i_img":500,
+            "i_print":100,
+            "i_testset":50000,
+            "i_video":50000,
+            "i_weights":10000,
+            "lindisp":False,
+            "llffhold":8,
+            "lrate":0.0005,
+            "lrate_decay":500,
+            "multires":10,
+            "multires_views":4,
+            "netchunk":65536,
+            "netdepth":8,
+            "netdepth_fine":8,
+            "netwidth":256,
+            "netwidth_fine":256,
+            "no_batching":True,
+            "no_ndc":False,
+            "no_reload":False,
+            "perturb":1.0,
+            "precrop_frac":0.5,
+            "precrop_iters":500,
+            "raw_noise_std":0.0,
+            "render_factor":0,
+            "render_only":False,
+            "render_test":False,
+            "shape":'greek',
+            "spherify":False,
+            "testskip":8,
+            "use_viewdirs":True,
+            "white_bkgd":True
+            }
 
-    parser.add_argument("--render_only", action='store_true', 
-                        help='do not optimize, reload weights and render out render_poses path')
-    parser.add_argument("--render_test", action='store_true', 
-                        help='render the test set instead of render_poses path')
-    parser.add_argument("--render_factor", type=int, default=0, 
-                        help='downsampling factor to speed up rendering, set 4 or 8 for fast preview')
+    args = argparse.Namespace(**settings)
+    return args
 
-    # training options
-    parser.add_argument("--precrop_iters", type=int, default=0,
-                        help='number of steps to train on central crops')
-    parser.add_argument("--precrop_frac", type=float,
-                        default=.5, help='fraction of img taken for central crops') 
+def get_nerf():
+    args = get_args()
 
-    # dataset options
-    parser.add_argument("--dataset_type", type=str, default='llff', 
-                        help='options: llff / blender / deepvoxels')
-    parser.add_argument("--testskip", type=int, default=8, 
-                        help='will load 1/N images from test/val sets, useful for large datasets like deepvoxels')
+    render_kwargs_train, render_kwargs_test, start, grad_vars, optimizer = create_nerf(args)
 
-    ## deepvoxels flags
-    parser.add_argument("--shape", type=str, default='greek', 
-                        help='options : armchair / cube / greek / vase')
+    chunk = args.chunk
+    hwf = None, None, None
+    K = None
+    renderer = Renderer(hwf, K, chunk, render_kwargs_train)
 
-    ## blender flags
-    parser.add_argument("--white_bkgd", action='store_true', 
-                        help='set to render synthetic data on a white bkgd (always use for dvoxels)')
-    parser.add_argument("--half_res", action='store_true', 
-                        help='load blender synthetic data at 400x400 instead of 800x800')
+    @typechecked
+    def nerf(points: TensorType["batch":..., 3]) -> TensorType["batch":...]:
+        out_shape = points.shape[:-1]
+        points = points.reshape(1, -1, 3)
+        output = renderer.get_density_from_pt(points)
+        return output.reshape(*out_shape)
 
-    ## llff flags
-    parser.add_argument("--factor", type=int, default=8, 
-                        help='downsample factor for LLFF images')
-    parser.add_argument("--no_ndc", action='store_true', 
-                        help='do not use normalized device coordinates (set for non-forward facing scenes)')
-    parser.add_argument("--lindisp", action='store_true', 
-                        help='sampling linearly in disparity rather than depth')
-    parser.add_argument("--spherify", action='store_true', 
-                        help='set for spherical 360 scenes')
-    parser.add_argument("--llffhold", type=int, default=8, 
-                        help='will take every 1/N images as LLFF test set, paper uses 8')
+    return nerf
 
-    # logging/saving options
-    parser.add_argument("--i_print",   type=int, default=100, 
-                        help='frequency of console printout and metric loggin')
-    parser.add_argument("--i_img",     type=int, default=500, 
-                        help='frequency of tensorboard image logging')
-    parser.add_argument("--i_weights", type=int, default=10000, 
-                        help='frequency of weight ckpt saving')
-    parser.add_argument("--i_testset", type=int, default=50000, 
-                        help='frequency of testset saving')
-    parser.add_argument("--i_video",   type=int, default=50000, 
-                        help='frequency of render_poses video saving')
+def main():
 
-    return parser
+    nerf = get_nerf()
+
+    side = 10
+    linspace = torch.linspace(-1,1, side)
+
+    # side, side, side, 3
+    coods = torch.stack( torch.meshgrid( linspace, linspace, linspace ), dim=-1)
+                
+    output = nerf(coods)
+
+    im = torch.mean( output, dim=1)
+    x_image = torch.mean( coods, dim=1)[...,0]
+    y_image = torch.mean( coods, dim=1)[...,2]
+
+    # +z in nerf is -y in blender
+    # +y in nerf is +z in blender
+    # +x in nerf is +x in blender
+
+    print("happy")
+    # exit()
+
+    plt.pcolormesh(x_image, y_image, im)
+    plt.show()
 
 
-parser = config_parser()
-args = parser.parse_args()
-
-# Namespace(N_importance=128, N_rand=1024, N_samples=64, basedir='./logs', chunk=32768, config='configs/playground.txt', datadir='./data/nerf_synthetic/playground', dataset_type='blender', expname='playground_test', factor=8, ft_path=None, half_res=False, i_embed=0, i_img=500, i_print=100, i_testset=50000, i_video=50000, i_weights=10000, lindisp=False, llffhold=8, lrate=0.0005, lrate_decay=500, multires=10, multires_views=4, netchunk=65536, netdepth=8, netdepth_fine=8, netwidth=256, netwidth_fine=256, no_batching=True, no_ndc=False, no_reload=False, perturb=1.0, precrop_frac=0.5, precrop_iters=500, raw_noise_std=0.0, render_factor=0, render_only=False, render_test=False, shape='greek', spherify=False, testskip=8, use_viewdirs=True, white_bkgd=True)
-
-
-# print(type(args))
-# print(args)
-
-render_kwargs_train, render_kwargs_test, start, grad_vars, optimizer = create_nerf(args)
-
-# render_args = {'hwf': hwf, 'K': K, 'chunk': args.chunk}
-# trajsavedir = os.path.join(basedir, expname, 'trajectory_viz')
-# os.makedirs(trajsavedir, exist_ok=True)
-
-# scene_dir = './scenes/' + expname + '/scene.gltf'
-
-# main_loop(P0, PT, T, N, N_iter, trajsavedir, render_args, render_kwargs_train, scene_dir)
-
-
-chunk = args.chunk
-hwf = None, None, None #render_args['hwf']
-K = None #render_args['K']
-
-renderer = Renderer(hwf, K, chunk, render_kwargs_train)
-
-# points = torch.tensor([ [0,0,0],
-#                [0.5, 0.5,0.5],
-#                [0.1, 0.1,0.1],
-#                [0.2, 0.2,0.2],
-#                [0.3, 0.3,0.3]])
-
-# points = points[None,...]
-
-side = 50
-
-linspace = torch.linspace(-1,1, side)
-
-# 50, 50, 50, 3
-coods = torch.stack( torch.meshgrid( linspace, linspace, linspace ), dim=-1)
-
-points = coods.reshape(-1, 3)[None,...]
-            
-output = (renderer.get_density_from_pt(points))
-
-
-output = output.reshape(side,side,side)
-
-im = torch.mean( output, dim=1)
-
-x_image = torch.mean( coods, dim=1)[...,0]
-y_image = torch.mean( coods, dim=1)[...,2]
-
-# +z in nerf is -y in blender
-# +y in nerf is +z in blender
-# +x in nerf is +x in blender
-
-plt.pcolormesh(x_image, y_image, im)
-plt.show()
-
+if __name__ == "__main__":
+    main()
 
 
 
