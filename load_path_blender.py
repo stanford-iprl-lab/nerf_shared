27c67e4ae5e31c31d8aeb25f3eeb12425564c271
Michal Adamkiewicz <mikh3x4@gmail.com>
2021-08-28 05:00:55 +0200

    blender laod with collections

--- load_path_blender.py
+++ load_path_blender.py
@@ -7,30 +7,52 @@ from mathutils import Matrix
 
 #filepath = bpy.path.abspath('//..') + "/playground_testing.json"
 
-
-save = 3
-filepath = bpy.path.abspath('//../paths/') +str(save)+"_testing.json"
-object_to_copy = bpy.data.objects["Cube"]
+object_to_copy = bpy.data.objects["Start"]
 
 
 path_parent = object_to_copy.copy()
 path_parent.name = "PATH"
 
-bpy.context.collection.objects.link(path_parent)
 
-with open(filepath) as f:
-    for i, line in enumerate(f):
+collection = bpy.data.collections.new("Diag")
+bpy.context.scene.collection.children.link(collection)
 
-        new_object = object_to_copy.copy()
-        bpy.context.collection.objects.link(new_object)
+collection.objects.link(path_parent)
 
-        pose = json.loads(line)
-        new_object.matrix_world = Matrix(pose)
-        print(i, pose)
+import os.path
 
-        new_object.name = "path_"+str(i)
-        new_object.parent = path_parent
-        new_object.matrix_parent_inverse = path_parent.matrix_world.inverted()
-        
-#bpy.context.scene.update()
-bpy.context.view_layer.update() 
+save = 0
+while True:
+    filepath = bpy.path.abspath('//../paths/') +str(save)+"_testing.json"
+    if not os.path.isfile(filepath):
+        break
+
+    bpy.context.scene.frame_set(save)
+
+    print("LOADING SAVE #"+str(save))
+    with open(filepath) as f:
+        for i, line in enumerate(f):
+
+            name = "path_"+str(i)
+            if save == 0:
+                new_object = object_to_copy.copy()
+                collection.objects.link(new_object)
+
+                new_object.name = name 
+                new_object.parent = path_parent
+                new_object.matrix_parent_inverse = path_parent.matrix_world.inverted()
+            else:
+                new_object = bpy.data.objects[name]
+
+            pose = json.loads(line)
+            new_object.matrix_world = Matrix(pose)
+            # print(i, pose)
+
+            new_object.keyframe_insert(data_path="location")
+            if new_object.rotation_mode == "QUATERNION":
+                new_object.keyframe_insert(data_path = 'rotation_quaternion')
+            else:
+                new_object.keyframe_insert(data_path = 'rotation_euler')
+            
+    bpy.context.view_layer.update() 
+    save += 1
