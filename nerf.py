a8455308d4d019f3071bf2ef31dbb4b15c9d855f
Michal Adamkiewicz <mikh3x4@gmail.com>
2021-10-01 12:37:06 -0700

    composite nerf

--- nerf.py
+++ nerf.py
@@ -11,10 +11,21 @@ class Nerf(torch.nn.Module):
         raise NotImplemented
 
     @typechecked
-    def forward(self, points: TensorType["dims":..., 3], view_direction: TensorType["dims":..., 3]):
+    def forward(self, points: TensorType["dims":..., 3], view_direction: TensorType["dims":..., 3]) -> TensorType["dims":...,4]:
         raise NotImplemented
 
     @typechecked
-    def density(self, points: TensorType["dims":..., 3]):
+    def density(self, points: TensorType["dims":..., 3]) -> TensorType["dims":...]:
         raise NotImplemented
 
+class CompositeNerf(Nerf):
+    def __init__(self, nerf_list, pose_list, cfg):
+        raise NotImplemented
+
+    @typechecked
+    def forward(self, points: TensorType["dims":..., 3], view_direction: TensorType["dims":..., 3]) -> TensorType["dims":...,4]:
+        raise NotImplemented
+
+    @typechecked
+    def density(self, points: TensorType["dims":..., 3]) -> TensorType["dims":...]:
+        raise NotImplemented
