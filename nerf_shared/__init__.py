5c5e7b7ba249ba7cfdf2a7a13bc702dbbbdb85d6
Preston Culbertson <pculbertson@stanford.edu>
2022-01-03 17:47:10 -0800

    reorganized into a Python package

---
+++ nerf_shared/__init__.py
@@ -0,0 +1,2 @@
+__all__ = ['config_parser', 'load_blender', 'load_deepvoxels', 'load_LINEMOD',
+          'load_llff', 'nerf', 'render_utils', 'utils']
---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name='nerf_shared', version='1.0', packages=['nerf_shared'])
